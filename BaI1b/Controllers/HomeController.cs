6bc4051e383256cd2f4ff7a71eb640ab91cbb710
NguyenNgocDuong257 <dwuong25072005@gmail.com>
2024-09-15 19:32:23 +0700

    Lab1b

---
+++ BaI1b/Controllers/HomeController.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace BaI1b.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult bai2ab()
+        {
+            return View();
+        }
+        public ActionResult bai2c()
+        {
+            return View();
+        }
+        public ActionResult bai3()
+        {
+            return View();
+        }
+        public ActionResult bai4()
+        {
+            return View();
+        }
+        public ActionResult bai5()
+        {
+            return View();
+        }
+        //public ActionResult About()
+        //{
+        //    ViewBag.Message = "Your application description page.";
+
+        //    return View();
+        //}
+
+        //public ActionResult Contact()
+        //{
+        //    ViewBag.Message = "Your contact page.";
+
+        //    return View();
+        //}
+    }
+}
